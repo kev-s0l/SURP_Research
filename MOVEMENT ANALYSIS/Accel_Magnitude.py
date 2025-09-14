8b12f9bc9e3182554777bed6d2f1ab353382da38
Kevin Bermejo <kevinbermejo4606@gmail.com>
2025-09-14 15:00:02 -0400

    Update Accel_Magnitude.py

--- MOVEMENT ANALYSIS/Accel_Magnitude.py
+++ MOVEMENT ANALYSIS/Accel_Magnitude.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 
-filename = r'c:\Users\kevin\OneDrive\Desktop\DATA - POSTER\DATASET - HIGH RISK 02\High_Risk02 [RAW MOVEMENT]\High Risk 02.h5'
-target_ID = 'XI-016162'  
+filename = r'path/to/your/file.h5'
+target_ID = 'Your_Sensor_ID'
 
 if not os.path.exists(filename):
     print(f"File '{filename}' not found in the current directory.")
@@ -66,5 +66,6 @@ if filename:
     output_plot_name = f"{os.path.splitext(filename)[0]}_acceleration_magnitude_plot.png"
     plt.tight_layout()
     plt.savefig(output_plot_name)
+
 
     print(f"Successfully generated and saved {output_plot_name}")
