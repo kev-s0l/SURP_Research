b76eeaf7ba559b3b470dd90ee510bb3796735ca0
Kevin Bermejo <kevinbermejo4606@gmail.com>
2025-09-14 15:00:27 -0400

    Update Bowely Skew_stat.py

--- MOVEMENT ANALYSIS/Bowely Skew_stat.py
+++ MOVEMENT ANALYSIS/Bowely Skew_stat.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 
-filename = r'c:\Users\kevin\OneDrive\Desktop\DATA - POSTER\DATASET - HIGH RISK 02\High_Risk02 [RAW MOVEMENT]\High Risk 02.h5'
-target_ID = 'XI-016162' 
+filename = r'path/to/your/file.h5'
+target_ID = 'Your_Sensor_ID'
 
 if not os.path.exists(filename):
     print(f"File '{filename}' not found in the current directory.")
@@ -76,3 +76,4 @@ if filename:
     output_plot_name = f"{os.path.splitext(filename)[0]}_bowley_skew_plot.png"
     plt.savefig(output_plot_name)
     print(f"Successfully generated and saved {output_plot_name}")
+
